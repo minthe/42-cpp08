031f396e3c6b03586a8eace0df929b863dcc19d5
vfuhlenb <vfuhlenb@students.42wolfsburg.de>
2023-05-16 17:50:53 +0200

    Span.class declared

--- ex01/include/Span.class.cpp
+++ ex01/include/Span.class.cpp
@@ -6,7 +6,16 @@
 /*   By: vfuhlenb <vfuhlenb@students.42wolfsburg    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/16 17:25:54 by vfuhlenb          #+#    #+#             */
-/*   Updated: 2023/05/16 17:25:55 by vfuhlenb         ###   ########.fr       */
+/*   Updated: 2023/05/16 17:46:46 by vfuhlenb         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "../include/Span.class.hpp"
+
+Span::Span() : _max_integers(0) {}
+
+Span::Span(unsigned int n) : _max_integers(n) {
+	
+}
+
+Span::~Span() {}
--- ex01/include/Span.class.hpp
+++ ex01/include/Span.class.hpp
@@ -6,7 +6,28 @@
 /*   By: vfuhlenb <vfuhlenb@students.42wolfsburg    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/16 17:25:53 by vfuhlenb          #+#    #+#             */
-/*   Updated: 2023/05/16 17:25:54 by vfuhlenb         ###   ########.fr       */
+/*   Updated: 2023/05/16 17:47:08 by vfuhlenb         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#ifndef SPAN_CLASS_HPP
+#define SPAN_CLASS_HPP
+
+class Span
+{
+	public:
+		Span(const unsigned int n);
+		Span(const Span& src);
+		Span&	operator=(const Span& rhs);
+		~Span();
+
+		void	addNumber(int value);
+		int		shortestSpan() const;
+		int		longestSpan() const;
+
+	private:
+		Span();
+		const unsigned int	_max_integers;
+};
+
+#endif
