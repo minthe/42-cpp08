c50aa3225841449c4a352961bd2d612ed39ebe7e
vfuhlenb <vfuhlenb@students.42wolfsburg.de>
2023-05-17 21:17:04 +0200

    feature complete

--- ex02/source/main.cpp
+++ ex02/source/main.cpp
@@ -6,35 +6,70 @@
 /*   By: vfuhlenb <vfuhlenb@students.42wolfsburg    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/17 19:35:57 by vfuhlenb          #+#    #+#             */
-/*   Updated: 2023/05/17 19:53:33 by vfuhlenb         ###   ########.fr       */
+/*   Updated: 2023/05/17 21:15:37 by vfuhlenb         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../include/MutantStack.class.hpp"
 #include <iostream>
+#include <list>
 
 int main()
 {
-	MutantStack<int> mstack;
-	mstack.push(5);
-	mstack.push(17);
-	std::cout << mstack.top() << std::endl;
-	mstack.pop();
-	std::cout << mstack.size() << std::endl;
-	mstack.push(3);
-	mstack.push(5);
-	mstack.push(737);
-	//[...]
-	mstack.push(0);
-	MutantStack<int>::iterator it = mstack.begin();
-	MutantStack<int>::iterator ite = mstack.end();
-	++it;
-	--it;
-	while (it != ite)
-	{
-	std::cout << *it << std::endl;
-	++it;
+	{	
+		std::cout << "\x1b[47mTest 1 (from subject)\x1b[0m\n"<< std::endl;
+
+		MutantStack<int> mstack;
+		mstack.push(5);
+		mstack.push(17);
+		std::cout << mstack.top() << std::endl;
+		mstack.pop();
+		std::cout << mstack.size() << std::endl;
+		mstack.push(3);
+		mstack.push(5);
+		mstack.push(737);
+		// [...]
+		mstack.push(0);
+		MutantStack<int>::iterator it = mstack.begin();
+		MutantStack<int>::iterator ite = mstack.end();
+		++it;
+		--it;
+		while (it != ite)
+		{
+		std::cout << *it << std::endl;
+		++it;
+		}
+		std::stack<int> s(mstack);
+		std::cout << "\ncomparing to stack 's'" << std::endl;
+		while (!s.empty())
+		{
+			std::cout << s.top() << std::endl;
+			s.pop();
+		}
 	}
-	std::stack<int> s(mstack);
+	{	
+		std::cout << "\n\x1b[47mTest 2\x1b[0m\n"<< std::endl;
+	
+		std::list<int> mstack;
+		mstack.push_back(5);
+		mstack.push_back(17);
+		std::cout << mstack.back() << std::endl;
+		mstack.pop_back();
+		std::cout << mstack.size() << std::endl;
+		mstack.push_back(3);
+		mstack.push_back(5);
+		mstack.push_back(737);
+		//[...]
+		mstack.push_back(0);
+		std::list<int>::iterator it = mstack.begin();
+		std::list<int>::iterator ite = mstack.end();
+		++it;
+		--it;
+		while (it != ite)
+		{
+		std::cout << *it << std::endl;
+		++it;
+		}
+	}
 	return 0;
 }
